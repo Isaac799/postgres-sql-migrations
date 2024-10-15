9515f33865b4328366b4aa9fc859f20d30cbc077
isaac lewis <mail@isaaclewis.me>
2024-10-14 19:17:24 -0500

    cli entry point

---
+++ cmd/migration/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"postgres_sql_migrations/internal/router"
+)
+
+func main() {
+	router.Router()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module postgres_sql_migrations
+
+go 1.23.2
+
+require github.com/lib/pq v1.10.9
