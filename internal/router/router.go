ce4d8f30fdf19b22530e863dc080232a0552953b
isaac lewis <mail@isaaclewis.me>
2024-10-14 19:20:40 -0500

    router
    based on flags given we route the request to a function that will handle
    the request accordingly
    
    handlers todo

---
+++ internal/router/router.go
@@ -0,0 +1,77 @@
+package router
+
+import (
+	"log"
+	"postgres_sql_migrations/internal/config"
+	"postgres_sql_migrations/internal/handlers"
+)
+
+func Router() {
+	flags := parseCliFlags()
+
+	if flags.Help {
+		handlers.ShowHelp()
+		return
+	}
+
+	if flags.GenMigration != "" {
+		if err := handlers.GenerateMigration(flags.GenMigration); err != nil {
+			log.Fatalf("Error generating migration: %v", err)
+		}
+		return
+	}
+
+	cfg, err := config.LoadConfig(flags.Env)
+	if err != nil {
+		log.Fatalf("%v\nConsider the example.env.bash\nMaybe you forgot '-e dev'?", err)
+	}
+
+	if flags.ListMigrations {
+		if err := handlers.ListAppliedMigrations(cfg); err != nil {
+			log.Fatalf("Error listing migrations: %v", err)
+		}
+		return
+	}
+
+	if flags.Summary {
+		if err := handlers.SummarizeMigrations(cfg); err != nil {
+			log.Fatalf("Error summarizing migrations: %v", err)
+		}
+		return
+	}
+
+	if flags.MigrateDry {
+		if err := handlers.MigrateDryRun(cfg); err != nil {
+			log.Fatalf("Error migrating: %v", err)
+		}
+		return
+	}
+
+	if flags.Create {
+		if err := handlers.CreateDatabase(cfg); err != nil {
+			log.Fatalf("Error creating database: %v", err)
+		}
+		return
+	}
+
+	if flags.Migrate {
+		if err := handlers.Migrate(cfg); err != nil {
+			log.Fatalf("Error migrating: %v", err)
+		}
+		return
+	}
+
+	if flags.FullReset {
+		if err := handlers.FullReset(cfg, flags.Env); err != nil {
+			log.Fatalf("Error performing full reset: %v", err)
+		}
+		return
+	}
+
+	if flags.Drop {
+		if err := handlers.DropDatabase(cfg, flags.Env); err != nil {
+			log.Fatalf("Error dropping database: %v", err)
+		}
+		return
+	}
+}
